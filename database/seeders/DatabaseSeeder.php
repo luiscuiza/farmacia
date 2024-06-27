5d7973b160cf307b362acedf99b39afe456b35e5
Luis Cuiza <luiscuiza128@gmail.com>
2024-06-27 00:08:33 -0400

    Seed Profiles & Users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,21 +2,31 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+use App\Models\User;
+use App\Models\Profile;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $admin = Profile::create(["name" => "Luis", "lastname" => "Cuiza"]);
+        $user = Profile::create(["name" => "Alfredo", "lastname" => "Duran"]);
+        User::create([
+            'name' => 'admin',
+            'role' => 'admin',
+            'email' => 'admin@pharmacy.com',
+            'password' => Hash::make('admin'),
+            'profile_id' => $admin->id
+        ]);
+        User::create([
+            'name' => 'user',
+            'role' => 'user',
+            'email' => 'user@pharmacy.com',
+            'password' => Hash::make('user'),
+            'profile_id' => $user->id
+        ]);
     }
 }
